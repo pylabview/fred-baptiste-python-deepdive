4e736d322e91b08b7f62b649922c923059215e5b
Rodrigo Arguello Serrano <rodrigo19662001@gmail.com>
2026-01-16 20:05:51 -0800

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   part1_practice/section_8_tuples_as_data_structures/124_namedtuples_extending_and_modifying.py

--- part1_practice/section_8_tuples_as_data_structures/124_namedtuples_extending_and_modifying.py
+++ part1_practice/section_8_tuples_as_data_structures/124_namedtuples_extending_and_modifying.py
@@ -1,6 +1,22 @@
+from collections import namedtuple
+
+
 def greetings():
     print("Hello namedtuples extending and modifying")
 
 
+Point2D = namedtuple("Point2D", "x y")
+Stock = namedtuple("Stock", "symbol year month day open high low close")
+
 if __name__ == "__main__":
     greetings()
+    pt = Point2D(10, 20)
+    print(f"pt {pt}, pt id: {id(pt)}")
+    pt = Point2D(100, pt.y)
+    print(f"pt {pt}, pt id: {id(pt)}")
+    djia = Stock("DJIA", 2018, 1, 25, 26_313, 26_458, 26_260, 26_392)
+    print(f"djia: {djia}")
+    *values, close = djia
+    print(f"*values: {values}, close: {close}")
+    djia = Stock(*values, 1000)
+    print(f"Modifying close value to 1000: {djia}")
